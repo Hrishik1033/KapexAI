97f73269687aab9ce257b12e8935be8f06a36588
Hrishik1033 <b25me1033@iitj.ac.in>
2026-07-25 21:30:15 +0530

    Changed file:agent , Added Folders: helpers,prompts

---
+++ worker/prompts/__init__.py
@@ -0,0 +1,9 @@
+from worker.prompts.system_prompt import SYSTEM_PROMPT, ANSWER_TEMPLATE
+from worker.prompts.query_generator import QUERY_GENERATOR_PROMPT, QUERY_GENERATOR_TEMPLATE
+
+__all__ = [
+    "SYSTEM_PROMPT",
+    "ANSWER_TEMPLATE",
+    "QUERY_GENERATOR_PROMPT",
+    "QUERY_GENERATOR_TEMPLATE",
+]
